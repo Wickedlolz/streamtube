4d1b1372da24e569216d32be2bb57a138d287284
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-12-12 10:15:25 +0200

    chore: add aria label on back button

--- src/components/PlayerBackButton.tsx
+++ src/components/PlayerBackButton.tsx
@@ -1,23 +1,24 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { CircleChevronLeft } from "lucide-react";
+import { useRouter } from 'next/navigation';
+import { CircleChevronLeft } from 'lucide-react';
 
 const PlayerBackButton = () => {
-  const navigate = useRouter();
+    const navigate = useRouter();
 
-  const handleBackClick = () => {
-    navigate.back(); // Go back to previous page
-  };
+    const handleBackClick = () => {
+        navigate.back(); // Go back to previous page
+    };
 
-  return (
-    <button
-      onClick={handleBackClick}
-      className="absolute z-10 text-5xl text-black bg-white m-3 md:m-4 rounded-full"
-    >
-      <CircleChevronLeft />
-    </button>
-  );
+    return (
+        <button
+            aria-label="Back Button"
+            onClick={handleBackClick}
+            className="absolute z-10 text-5xl text-black bg-white m-3 md:m-4 rounded-full"
+        >
+            <CircleChevronLeft />
+        </button>
+    );
 };
 
 export default PlayerBackButton;
