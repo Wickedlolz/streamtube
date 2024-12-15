7f5664e55b229028d277db161bba553f31e39d75
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-12-15 15:07:19 +0200

    chore: update descripton meta tag in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: './fonts/GeistVF.woff',
+    variable: '--font-geist-sans',
+    weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: './fonts/GeistMonoVF.woff',
+    variable: '--font-geist-mono',
+    weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "StreamTube",
-  description:
-    "StreamTube is your go-to platform for exploring and streaming movies. Discover detailed information on films and actors, curate favorites, and enjoy a personalized movie experience.",
+    title: 'StreamTube',
+    description:
+        'StreamTube: Your ultimate platform for exploring and streaming movies. Access detailed information about films and actors, curate your favorite list, and enjoy an exceptional movie experience—all completely FREE!',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        cz-shortcut-listen="true"
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                cz-shortcut-listen="true"
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
