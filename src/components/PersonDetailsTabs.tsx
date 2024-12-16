8b98b8171feba946169d8f9a484b68d3b7a8ae75
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-12-16 19:58:32 +0200

    feat: add dialog for image preview of person ✅

--- src/components/PersonDetailsTabs.tsx
+++ src/components/PersonDetailsTabs.tsx
@@ -1,68 +1,100 @@
-import Link from "next/link";
-import Image from "next/image";
-import { getImagePath } from "@/lib/getImagePath";
-import { IPerson } from "@/interfaces/people";
+import Link from 'next/link';
+import Image from 'next/image';
+import { getImagePath } from '@/lib/getImagePath';
+import { IPerson } from '@/interfaces/people';
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+    Dialog,
+    DialogDescription,
+    DialogTitle,
+    DialogContent,
+    DialogTrigger,
+    DialogHeader,
+} from '@/components/ui/dialog';
 
 type PersonTabbedSectionsType = {
-  data: IPerson | undefined;
+    data: IPerson | undefined;
 };
 
 const PersonDetailsTabs = ({ data }: PersonTabbedSectionsType) => {
-  return (
-    <Tabs defaultValue="filmography" className="w-full mt-8">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="filmography">Filmography</TabsTrigger>
-        <TabsTrigger value="gallery">Gallery</TabsTrigger>
-      </TabsList>
-      <TabsContent value="filmography">
-        <div className="space-y-4">
-          {data?.movie_credits?.cast.map((movie) => (
-            <Card key={movie.id}>
-              <CardContent className="flex items-center p-4">
-                <Image
-                  src={getImagePath(movie.poster_path)}
-                  alt={movie.title}
-                  width={50}
-                  height={75}
-                  className="object-cover rounded mr-4"
-                  loading="lazy"
-                />
-                <div>
-                  <Link
-                    href={`/movie/${movie.id}/${encodeURIComponent(
-                      movie.title
-                    )}`}
-                    className="font-semibold hover:underline"
-                  >
-                    {movie.title}
-                  </Link>
-                  <p className="text-sm text-gray-500">{movie.release_date}</p>
-                  <p className="text-sm">{movie.character}</p>
+    return (
+        <Tabs defaultValue="filmography" className="w-full mt-8">
+            <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="filmography">Filmography</TabsTrigger>
+                <TabsTrigger value="gallery">Gallery</TabsTrigger>
+            </TabsList>
+            <TabsContent value="filmography">
+                <div className="space-y-4">
+                    {data?.movie_credits?.cast.map((movie) => (
+                        <Card key={movie.id}>
+                            <CardContent className="flex items-center p-4">
+                                <Image
+                                    src={getImagePath(movie.poster_path)}
+                                    alt={movie.title}
+                                    width={50}
+                                    height={75}
+                                    className="object-cover rounded mr-4"
+                                    loading="lazy"
+                                />
+                                <div>
+                                    <Link
+                                        href={`/movie/${
+                                            movie.id
+                                        }/${encodeURIComponent(movie.title)}`}
+                                        className="font-semibold hover:underline"
+                                    >
+                                        {movie.title}
+                                    </Link>
+                                    <p className="text-sm text-gray-500">
+                                        {movie.release_date}
+                                    </p>
+                                    <p className="text-sm">{movie.character}</p>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))}
                 </div>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </TabsContent>
-      <TabsContent value="gallery">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {data?.images.profiles.map((profile, index) => (
-            <Image
-              key={index}
-              src={`${getImagePath(profile.file_path)}`}
-              alt={`Gallery image ${data.name} ${index}`}
-              width={200}
-              height={200}
-              className="object-cover rounded-lg"
-            />
-          ))}
-        </div>
-      </TabsContent>
-    </Tabs>
-  );
+            </TabsContent>
+            <TabsContent value="gallery">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {data?.images.profiles.map((profile, index) => (
+                        <Dialog key={index}>
+                            <DialogTrigger>
+                                <Image
+                                    key={index}
+                                    src={`${getImagePath(profile.file_path)}`}
+                                    alt={`Gallery image ${data.name} ${index}`}
+                                    width={200}
+                                    height={200}
+                                    className="object-cover rounded-lg"
+                                />
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>{data.name}</DialogTitle>
+                                    <DialogDescription asChild>
+                                        <Image
+                                            key={index}
+                                            src={getImagePath(
+                                                profile.file_path,
+                                                true
+                                            )}
+                                            width={400}
+                                            height={500}
+                                            alt={`Gallery image ${data.name} ${index}`}
+                                            className="w-full h-auto max-w-[90vw] md:max-w-[80vw] lg:max-w-[50vw] max-h-[70vh] object-contain rounded-lg"
+                                        />
+                                    </DialogDescription>
+                                </DialogHeader>
+                            </DialogContent>
+                        </Dialog>
+                    ))}
+                </div>
+            </TabsContent>
+        </Tabs>
+    );
 };
 
 export default PersonDetailsTabs;
--- src/components/YouTubeVideos.tsx
+++ src/components/YouTubeVideos.tsx
@@ -1,49 +1,50 @@
-"use client";
+'use client';
 
-import YouTube, { YouTubeProps } from "react-youtube";
-import { IVideoProps } from "@/interfaces/movie";
+import YouTube, { YouTubeProps } from 'react-youtube';
+import { IVideoProps } from '@/interfaces/movie';
 
 type IVideoPlayerProps = {
-  videos: IVideoProps[];
+    videos: IVideoProps[];
 };
 
 const YouTubeVideos = ({ videos }: IVideoPlayerProps) => {
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    event.target.pauseVideo();
-  };
+    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+        event.target.pauseVideo();
+    };
 
-  const opts = {
-    height: "300px",
-    width: "100%",
-  };
+    const opts = {
+        height: '300px',
+        width: '100%',
+    };
 
-  return (
-    <article>
-      <h2 className="text-xl font-bold mb-5 dark:text-white">
-        Offical videos from Youtube:
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {videos
-          ?.filter((v) => v.type === "Trailer")
-          .slice(0, 5)
-          .map((video) => (
-            <div
-              key={video?.id}
-              className="border border-gray-600 rounded-md w-full overflow-hidden relative"
-            >
-              <p className="text-sm font-medium px-6 py-3 dark:text-white">
-                Type: {video?.type} - {video?.official ? "Offical" : "General"}
-              </p>
-              <YouTube
-                videoId={video?.key}
-                onReady={onPlayerReady}
-                opts={opts}
-              />
+    return (
+        <article>
+            <h2 className="text-xl font-bold mb-5 dark:text-white">
+                Offical videos from Youtube:
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {videos
+                    ?.filter((v) => v.type === 'Trailer')
+                    .slice(0, 5)
+                    .map((video) => (
+                        <div
+                            key={video?.id}
+                            className="border border-gray-600 rounded-md w-full overflow-hidden relative"
+                        >
+                            <p className="text-sm font-medium px-6 py-3 dark:text-white">
+                                Type: {video?.type} -{' '}
+                                {video?.official ? 'Offical' : 'General'}
+                            </p>
+                            <YouTube
+                                videoId={video?.key}
+                                onReady={onPlayerReady}
+                                opts={opts}
+                            />
+                        </div>
+                    ))}
             </div>
-          ))}
-      </div>
-    </article>
-  );
+        </article>
+    );
 };
 
 export default YouTubeVideos;
