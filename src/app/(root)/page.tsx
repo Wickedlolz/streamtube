9da58c7a7df6481a117a1f9c8f1611b7154e7c09
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-12-31 15:18:22 +0200

    feat: add welcome component with some description about application

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,27 +1,32 @@
-import { Metadata } from "next";
-import { endpoints } from "@/lib/endpoints";
+import { Metadata } from 'next';
+import { endpoints } from '@/lib/endpoints';
 
-import HeroCarousel from "@/components/HeroCarousel";
-import Highlights from "@/components/Highlights";
-import MovieContainer from "@/components/MovieContainer";
-import PopularNetworks from "@/components/PopularNetworks";
+import HeroCarousel from '@/components/HeroCarousel';
+import Highlights from '@/components/Highlights';
+import MovieContainer from '@/components/MovieContainer';
+import PopularNetworks from '@/components/PopularNetworks';
+import Welcome from '@/components/Welcome';
 
 export const metadata: Metadata = {
-  title: "Home | StreamTube",
+    title: 'Home | StreamTube',
 };
 
 export default function Home() {
-  return (
-    <section>
-      <HeroCarousel />
-      <section className="flex flex-col space-y-2">
-        <Highlights />
-        <MovieContainer url={endpoints.nowPlaying} title="Now Playing" />
-        <PopularNetworks />
-        <MovieContainer url={endpoints.upcoming} title="Upcoming" />
-        <MovieContainer url={endpoints.topRated} title="Top Rated" />
-        <MovieContainer url={endpoints.popular} title="Popular" />
-      </section>
-    </section>
-  );
+    return (
+        <section>
+            <HeroCarousel />
+            <Welcome />
+            <section className='flex flex-col space-y-2'>
+                <Highlights />
+                <MovieContainer
+                    url={endpoints.nowPlaying}
+                    title='Now Playing'
+                />
+                <PopularNetworks />
+                <MovieContainer url={endpoints.upcoming} title='Upcoming' />
+                <MovieContainer url={endpoints.topRated} title='Top Rated' />
+                <MovieContainer url={endpoints.popular} title='Popular' />
+            </section>
+        </section>
+    );
 }
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,18 @@
+const Welcome = () => {
+    return (
+        <section className='p-8 bg-gradient-to-r from-red-600 to-gray-900 shadow-xl text-white'>
+            <h1 className='text-3xl font-extrabold mb-6'>
+                Welcome to StreamTube
+            </h1>
+            <p className='text-lg leading-relaxed'>
+                StreamTube is your ultimate destination for streaming the latest
+                and greatest movies. From now playing hits to upcoming
+                blockbusters, top-rated classics, and popular favorites, we have
+                it all. Enjoy a seamless and immersive viewing experience with
+                our curated collections.
+            </p>
+        </section>
+    );
+};
+
+export default Welcome;
