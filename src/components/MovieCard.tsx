0a6e0ded92e32f117ab009afa3f55ad2915468d6
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-12-31 14:55:43 +0200

    chore: remove all usige of memo (in react 19 not need to use this)

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,36 +1,36 @@
-import Link from "next/link";
-import { IMovie, IMovieDetails } from "@/interfaces/movie";
-import Image from "next/image";
-import { getImagePath } from "@/lib/getImagePath";
+import Link from 'next/link';
+import Image from 'next/image';
+import { IMovie, IMovieDetails } from '@/interfaces/movie';
+import { getImagePath } from '@/lib/getImagePath';
 
 type MovieCardProps = {
-  movie: IMovie | IMovieDetails;
+    movie: IMovie | IMovieDetails;
 };
 
 const MovieCard = ({ movie }: MovieCardProps) => {
-  return (
-    <Link
-      href={`/movie/${movie?.id}/${encodeURIComponent(
-        movie.title.toLocaleLowerCase().split(" ").join("-")
-      )}`}
-      className="relative flex-shrink-0 cursor-pointer transform transition-transform duration-300 ease-out hover:scale-105 hover:shadow-xl hover:shadow-gray-900/50"
-      aria-label={`View details for ${movie.title}`}
-    >
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/40 to-black/80 z-10" />
+    return (
+        <Link
+            href={`/movie/${movie?.id}/${encodeURIComponent(
+                movie.title.toLocaleLowerCase().split(' ').join('-')
+            )}`}
+            className='relative flex-shrink-0 cursor-pointer transform transition-transform duration-300 ease-out hover:scale-105 hover:shadow-xl hover:shadow-gray-900/50'
+            aria-label={`View details for ${movie.title}`}
+        >
+            <div className='absolute inset-0 bg-gradient-to-b from-transparent via-black/40 to-black/80 z-10' />
 
-      <p className="absolute z-20 bottom-5 left-5 text-white text-sm lg:text-base font-medium line-clamp-1">
-        {movie?.title}
-      </p>
-      <Image
-        src={getImagePath(movie?.backdrop_path)}
-        alt={movie.title}
-        width={1920}
-        height={1080}
-        className="w-fit h-48 sm:h-56 lg:min-w-[400px] object-cover shadow-md shadow-gray-900 drop-shadow-xl"
-        loading="lazy"
-      />
-    </Link>
-  );
+            <p className='absolute z-20 bottom-5 left-5 text-white text-sm lg:text-base font-medium line-clamp-1'>
+                {movie?.title}
+            </p>
+            <Image
+                src={getImagePath(movie?.backdrop_path)}
+                alt={movie.title}
+                width={1920}
+                height={1080}
+                className='w-fit h-48 sm:h-56 lg:min-w-[400px] object-cover shadow-md shadow-gray-900 drop-shadow-xl'
+                loading='lazy'
+            />
+        </Link>
+    );
 };
 
 export default MovieCard;
--- src/components/MovieOverview.tsx
+++ src/components/MovieOverview.tsx
@@ -1,36 +1,36 @@
-"use client";
+'use client';
 
-import { IMovieDetails } from "@/interfaces/movie";
-import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { useState } from 'react';
+import { IMovieDetails } from '@/interfaces/movie';
+import { cn } from '@/lib/utils';
 
 type MovieOverviewProps = {
-  movie: IMovieDetails;
+    movie: IMovieDetails;
 };
 
 export default function MovieOverview({ movie }: MovieOverviewProps) {
-  const [show, setShow] = useState<boolean>(false);
+    const [show, setShow] = useState<boolean>(false);
 
-  const toggleShow = () => setShow((state) => !state);
+    const toggleShow = () => setShow((state) => !state);
 
-  return (
-    <p className="sm:text-base xs:text-[15.75px] text-[14.25px] leading-relaxed">
-      <span>
-        {movie?.overview?.length > 280 && !show
-          ? `${movie?.overview.slice(0, 280)}...`
-          : movie?.overview}
-      </span>
-      <button
-        type="button"
-        className={cn(
-          `font-bold ml-1 hover:underline transition-all duration-300`,
-          movie!.overview.length > 280 ? "inline-block" : "hidden"
-        )}
-        onClick={toggleShow}
-        aria-expanded={show ? "true" : "false"}
-      >
-        {!show ? "show more" : "show less"}
-      </button>
-    </p>
-  );
+    return (
+        <p className='sm:text-base xs:text-[15.75px] text-[14.25px] leading-relaxed'>
+            <span>
+                {movie?.overview?.length > 280 && !show
+                    ? `${movie?.overview.slice(0, 280)}...`
+                    : movie?.overview}
+            </span>
+            <button
+                type='button'
+                className={cn(
+                    `font-bold ml-1 hover:underline transition-all duration-300`,
+                    movie!.overview.length > 280 ? 'inline-block' : 'hidden'
+                )}
+                onClick={toggleShow}
+                aria-expanded={show ? 'true' : 'false'}
+            >
+                {!show ? 'show more' : 'show less'}
+            </button>
+        </p>
+    );
 }
