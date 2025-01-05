572dc4bba6900dc23fdf4a29d77a0af13f646fd2
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2025-01-05 06:42:41 +0200

    refactor: update useInfiniteScroll hook to use QueryKey and improve pagination handling ✅

--- src/components/GenreMoviesWithInfinityScroll.tsx
+++ src/components/GenreMoviesWithInfinityScroll.tsx
@@ -1,67 +1,75 @@
-"use client";
+'use client';
 
-import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
-import { endpoints } from "@/lib/endpoints";
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
+import { endpoints } from '@/lib/endpoints';
 
-import MovieCard from "./MovieCard";
-import Spinner from "./Spinner";
+import MovieCard from './MovieCard';
+import Spinner from './Spinner';
+import { Button } from './ui/button';
 
 type GenreMoviesWithInfinityScrollProps = {
-  genre: string;
-  genreId: string;
+    genre: string;
+    genreId: string;
 };
 
 const GenreMoviesWithInfinityScroll = ({
-  genre,
-  genreId,
+    genre,
+    genreId,
 }: GenreMoviesWithInfinityScrollProps) => {
-  const url = new URL(endpoints.discoverMovies);
-  if (genreId) {
-    url.searchParams.set("with_genres", genreId);
-  }
+    const url = new URL(endpoints.discoverMovies);
+    if (genreId) {
+        url.searchParams.set('with_genres', genreId);
+    }
 
-  const { data, isLoading, isFetchingNextPage } = useInfiniteScroll(
-    url,
-    "genre",
-    genre
-  );
+    const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
+        useInfiniteScroll(url, ['genre'], genre);
 
-  if (isLoading) {
+    if (isLoading) {
+        return (
+            <section className='py-10 max-w-screen-xl mx-auto'>
+                <Spinner />
+            </section>
+        );
+    }
+
     return (
-      <section className="py-10 max-w-screen-xl mx-auto">
-        <Spinner />
-      </section>
+        <div className='flex  overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide flex-col space-x-0 space-y-12'>
+            {data?.pages?.map(({ results }) => {
+                return results.map((movie) => (
+                    <div
+                        key={movie.id}
+                        className={
+                            'flex flex-col space-y-5 mb-5 items-center lg:flex-row space-x-5'
+                        }
+                    >
+                        <MovieCard movie={movie} />
+                        <div className='max-w-2xl'>
+                            <p className='font-bold dark:text-white'>
+                                {movie?.title} (
+                                {movie?.release_date?.split('-')[0]})
+                            </p>
+                            <hr className='mb-3' />
+                            <p className='dark:text-white'>{movie?.overview}</p>
+                        </div>
+                    </div>
+                ));
+            })}
+            {isFetchingNextPage && (
+                <p className='text-white font-bold text-center'>
+                    Loading next movies&hellip;
+                </p>
+            )}
+            {hasNextPage && (
+                <Button
+                    onClick={() => fetchNextPage()}
+                    className='block mx-auto mt-5 px-6 py-2 bg-red-600 text-white font-bold rounded-lg transition-colors duration-300 hover:bg-red-700 disabled:bg-gray-500 disabled:cursor-not-allowed'
+                    disabled={isFetchingNextPage}
+                >
+                    {isFetchingNextPage ? 'Loading...' : 'Load More'}
+                </Button>
+            )}
+        </div>
     );
-  }
-
-  return (
-    <div className="flex  overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide flex-col space-x-0 space-y-12">
-      {data?.pages?.map(({ results }) => {
-        return results.map((movie) => (
-          <div
-            key={movie.id}
-            className={
-              "flex flex-col space-y-5 mb-5 items-center lg:flex-row space-x-5"
-            }
-          >
-            <MovieCard movie={movie} />
-            <div className="max-w-2xl">
-              <p className="font-bold dark:text-white">
-                {movie?.title} ({movie?.release_date?.split("-")[0]})
-              </p>
-              <hr className="mb-3" />
-              <p className="dark:text-white">{movie?.overview}</p>
-            </div>
-          </div>
-        ));
-      })}
-      {isFetchingNextPage && (
-        <p className="text-white font-bold text-center">
-          Loading next movies&hellip;
-        </p>
-      )}
-    </div>
-  );
 };
 
 export default GenreMoviesWithInfinityScroll;
--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -1,59 +1,27 @@
-import { useEffect, useState } from 'react';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { QueryKey, useInfiniteQuery } from '@tanstack/react-query';
 import { requester } from '@/lib/utils';
 import { ISearchResults } from '@/interfaces/movie';
 
 export const useInfiniteScroll = (
     url: URL,
-    qKey: string,
+    qKey: QueryKey,
     qValue: string | null | undefined
 ) => {
-    const [page, setPage] = useState<number>(1);
-
-    useEffect(() => {
-        setPage(1); // Reset page to 1 on search query change
-    }, [qValue]);
-
     const { data, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage } =
         useInfiniteQuery({
             queryKey: [qKey, qValue],
-            queryFn: ({ pageParam = page }) => handleNextPage(pageParam),
-            initialPageParam: 1,
-            getNextPageParam: (lastPage, allPages) => {
-                return lastPage.total_pages &&
-                    allPages.length < lastPage.total_pages
-                    ? allPages.length + 1
-                    : undefined;
+            queryFn: async ({ pageParam = 1 }) => {
+                url.searchParams.set('page', pageParam.toString());
+                return await requester<ISearchResults>(url);
             },
+            initialPageParam: 1,
+            getNextPageParam: (lastPage, allPages) =>
+                allPages.length < (lastPage.total_pages || 0)
+                    ? allPages.length + 1
+                    : undefined,
             enabled: !!qValue,
             refetchOnMount: true,
         });
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isFetchingNextPage, hasNextPage]);
-
-    const handleNextPage = async (lastPage: number) => {
-        setPage(lastPage);
-        url.searchParams.set('page', lastPage.toString());
-        return await requester<ISearchResults>(url);
-    };
-
-    const handleScroll = () => {
-        if (
-            window.innerHeight + window.scrollY >=
-                document.documentElement.scrollHeight - 1200 &&
-            !isFetchingNextPage &&
-            hasNextPage
-        ) {
-            fetchNextPage();
-        }
-    };
-
-    return { data, isLoading, isFetchingNextPage };
+    return { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage };
 };
