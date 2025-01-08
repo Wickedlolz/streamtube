19ef33f059447eb437b69c80863b81c3e205ff52
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2025-01-08 10:26:09 +0200

    refactor: update markup in SearchMoviesWithInfinityScroll component for consistency

--- src/components/SearchMoviesWithInfinityScroll.tsx
+++ src/components/SearchMoviesWithInfinityScroll.tsx
@@ -21,49 +21,49 @@ const SearchMoviesWithInfinityScroll = ({
 
     if (isLoading) {
         return (
-            <section className='py-10 max-w-screen-xl mx-auto'>
+            <section className="py-10 max-w-screen-xl mx-auto">
                 <Spinner />
             </section>
         );
     }
 
     return (
-        <div className='flex  overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide flex-col space-x-0 space-y-12'>
+        <section className="flex  overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide flex-col space-x-0 space-y-12">
             {data?.pages?.map(({ results }) => {
                 return results.map((movie) => (
-                    <div
+                    <article
                         key={movie.id}
                         className={
                             'flex flex-col space-y-5 mb-5 items-center lg:flex-row space-x-5'
                         }
                     >
                         <MovieCard movie={movie} />
-                        <div className='max-w-2xl'>
-                            <p className='font-bold dark:text-white'>
+                        <div className="max-w-2xl">
+                            <p className="font-bold dark:text-white">
                                 {movie?.title} (
                                 {movie?.release_date?.split('-')[0]})
                             </p>
-                            <hr className='mb-3' />
-                            <p className='dark:text-white'>{movie?.overview}</p>
+                            <hr className="mb-3" />
+                            <p className="dark:text-white">{movie?.overview}</p>
                         </div>
-                    </div>
+                    </article>
                 ));
             })}
             {isFetchingNextPage && (
-                <p className='text-white font-bold text-center'>
+                <p className="text-white font-bold text-center">
                     Loading next movies&hellip;
                 </p>
             )}
             {hasNextPage && (
                 <Button
                     onClick={() => fetchNextPage()}
-                    className='block mx-auto mt-5 px-6 py-2 bg-red-600 text-white font-bold rounded-lg transition-colors duration-300 hover:bg-red-700 disabled:bg-gray-500 disabled:cursor-not-allowed'
+                    className="block mx-auto mt-5 px-6 py-2 bg-red-600 text-white font-bold rounded-lg transition-colors duration-300 hover:bg-red-700 disabled:bg-gray-500 disabled:cursor-not-allowed"
                     disabled={isFetchingNextPage}
                 >
                     {isFetchingNextPage ? 'Loading...' : 'Load More'}
                 </Button>
             )}
-        </div>
+        </section>
     );
 };
 
