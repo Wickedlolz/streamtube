f00ff4427f0e86a8d619507598d3d65706ffb294
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2025-01-11 10:34:28 +0200

    style: add fixed heigth and overflow-y for scrolling

--- src/components/GenreDropDown.tsx
+++ src/components/GenreDropDown.tsx
@@ -1,43 +1,43 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useQuery } from "@tanstack/react-query";
-import { loadGenres } from "@/lib/actions";
+import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
+import { loadGenres } from '@/lib/actions';
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { ChevronDown } from 'lucide-react';
 
 const GenreDropDown = () => {
-  const { data } = useQuery({
-    queryKey: ["movieGenres"],
-    queryFn: () => loadGenres(),
-  });
+    const { data } = useQuery({
+        queryKey: ['movieGenres'],
+        queryFn: () => loadGenres(),
+    });
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="text-white flex items-center text-sm font-medium">
-        Genre <ChevronDown className="ml-1" size={20} />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        {data?.genres?.map((genre) => (
-          <DropdownMenuItem key={genre?.id}>
-            <Link href={`/genre/${genre?.id}?genre=${genre.name}`}>
-              {genre?.name}
-            </Link>
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="text-white flex items-center text-sm font-medium">
+                Genre <ChevronDown className="ml-1" size={20} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="max-h-96 overflow-y-auto">
+                <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {data?.genres?.map((genre) => (
+                    <DropdownMenuItem key={genre?.id}>
+                        <Link href={`/genre/${genre?.id}?genre=${genre.name}`}>
+                            {genre?.name}
+                        </Link>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 };
 
 export default GenreDropDown;
